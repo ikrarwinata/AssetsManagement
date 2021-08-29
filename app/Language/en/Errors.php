19a598cd4a02a4852cc20b0658b58dc9573e5f6e
ikrarwinata@gmail.com <ikrarwinata@gmail.com>
2021-08-29 17:44:08 +0700

    .

--- app/Language/en/Errors.php
+++ app/Language/en/Errors.php
@@ -8,7 +8,7 @@ return [
     'PasswordMissing'				=> 'Password  cannot be empty !',
     'PasswordMismatch'			 	=> 'Invalid password',
     'PasswordConfirmMismatch'		=> 'Password confirmation mismatch',
-    'LoginFailed' 					=> 'Username or Password you given is not found',
+    'LoginFailed' 					=> 'Username or Password is not incorrect',
     'DataNotExist'			 		=> 'Sorry... This data is missing !',
     'DataDuplicate'                => 'Sorry... This data already exist !',
     'FileNotExist'                 => 'Sorry... This file is missing !',
